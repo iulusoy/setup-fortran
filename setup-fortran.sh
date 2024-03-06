934c95aa6f716bbf74300b66d1b123f77a7a9475
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-03-06 13:59:13 +0100

    test 2021.5 only

--- setup-fortran.sh
+++ setup-fortran.sh
@@ -468,20 +468,22 @@ install_intel_dmg()
 
   if $install_mkl; then
     if [ "$MACOS_BASEKIT_URL" == "" ]; then
-      "ERROR: MACOS_BASEKIT_URL is empty - please check the version mapping for MKL"
+      echo "ERROR: MACOS_BASEKIT_URL is empty - please check the version mapping for MKL"
+      echo "SKIPPING MKL installation..."
+    else
+      require_fetch
+      $fetch $MACOS_BASEKIT_URL > m_BASEKit.dmg
+      ls -lh
+      hdiutil verify m_BASEKit.dmg
+      hdiutil attach m_BASEKit.dmg
+      sudo /Volumes/"$(basename "$MACOS_BASEKIT_URL" .dmg)"/bootstrapper.app/Contents/MacOS/bootstrapper -s \
+        --action install \
+        --eula=accept \
+        --continue-with-optional-error=yes \
+        --log-dir=.
+      hdiutil detach /Volumes/"$(basename "$MACOS_BASEKIT_URL" .dmg)" -quiet
+      rm m_BASEKit.dmg
     fi
-    require_fetch
-    $fetch $MACOS_BASEKIT_URL > m_BASEKit.dmg
-    ls -lh
-    hdiutil verify m_BASEKit.dmg
-    hdiutil attach m_BASEKit.dmg
-    sudo /Volumes/"$(basename "$MACOS_BASEKIT_URL" .dmg)"/bootstrapper.app/Contents/MacOS/bootstrapper -s \
-      --action install \
-      --eula=accept \
-      --continue-with-optional-error=yes \
-      --log-dir=.
-    hdiutil detach /Volumes/"$(basename "$MACOS_BASEKIT_URL" .dmg)" -quiet
-    rm m_BASEKit.dmg
   fi
 
   require_fetch
